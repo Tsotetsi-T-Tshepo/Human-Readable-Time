c6763ed142fcc12dcacbd47cd5c237b65dc7becd
Tshepo Tsotetsi <tsotetsitshepo60@gmail.com>
2022-02-03 18:27:00 +0200

    Add project files.

---
+++ Human Readable Time/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Human_Readable_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GetReadableTime(60));
+        }
+    }
+    class Kata
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int timeInSeconds = seconds, timeInMinuetes=0, timeInHours=0;
+
+            if (seconds>=60)
+            {
+                timeInMinuetes = seconds / 60;
+                timeInSeconds = timeInSeconds % 60;
+            }
+
+            if(timeInMinuetes>60)
+            {
+                timeInHours = timeInMinuetes / 60;
+                timeInMinuetes = timeInMinuetes % 60;
+            }
+
+            return $"{timeInHours.ToString("D2")}:{timeInMinuetes.ToString("D2")}:{timeInSeconds.ToString("D2")}";
+
+
+
+        }
+    }
+}
